eaf4dd33a9ca9d90da9f6f17ef597ab2466087e2
Numpienick <211162@edu.rocfriesepoort.nl>
2018-10-02 13:34:31 +0200

    Trying some shit with enemies

--- Clash of the Empires/Assets/Scripts/Units.cs
+++ Clash of the Empires/Assets/Scripts/Units.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Units : OffensivePlaceables
 {
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void Movement()
+    private SphereCollider detectingRange;
+    void Start()
     {
+        detectingRange = GetComponent<SphereCollider>();
+        healthbarOffsetY = 6;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(6);
+
+        UpdateHealthBarPosition();
+        healthFill.value = currentHealth / maxHealth;
+    }
+
+    void DealDamage(float damageValue)
+    {
+        currentHealth -= damageValue;
+        Debug.Log("ouch");
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
     }
 }
