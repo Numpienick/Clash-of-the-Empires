88a6875780463898c1a4f5d2a24124ca71f50e2e
Numpienick <211162@edu.rocfriesepoort.nl>
2018-10-04 14:37:23 +0200

    Fixed collider issues and the archer can 'shoot'

--- Clash of the Empires/Assets/Scripts/UnitScripts/Archer.cs
+++ Clash of the Empires/Assets/Scripts/UnitScripts/Archer.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Archer : OffensivePlaceables
 {
+    public GameObject Bullet;
+    public GameObject Bullet_Emitter;
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Shoot();
+        }
+    }
+    public void Shoot()
+    {
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+       // Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 180);
+
+        Rigidbody Temporary_RigidBody;
+        Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+        Destroy(Temporary_Bullet_Handler, 10.0f);
+    }
 }
