b953bcd9a601ae537c4eba545c10304bbf7f77f6
Melle Romp <211333@edu.rocfriesepoort.nl>
2018-10-09 13:21:48 +0200

    yodieyo

--- Clash of the Empires/Assets/Scripts/cameraMovement.cs
+++ Clash of the Empires/Assets/Scripts/cameraMovement.cs
@@ -11,49 +11,24 @@ public class cameraMovement : MonoBehaviour {
     public float pivotSpeed = 100f;
 
     void Update () {
-        //panSpeed = 20f;
+        
         Vector3 pos = transform.position;
+        //movement(wasd)
+        transform.Translate(Input.GetAxis("Horizontal") * panSpeed * Time.deltaTime * Vector3.right);
+        transform.Translate(Input.GetAxis("Vertical") * panSpeed * Time.deltaTime * Vector3.forward);
+        //rotate camera(qe)
+        transform.Rotate(Input.GetAxis("Rotate") * pivotSpeed * Vector3.down * Time.deltaTime);
 
-		if (Input.GetKey("s"))
-        {
-            transform.Translate (panSpeed *  Vector3.back * Time.deltaTime);
-        }
-
-        if (Input.GetKey("w"))
-        {
-           transform.Translate (panSpeed *  Vector3.forward * Time.deltaTime);
-        }
-
-        if (Input.GetKey("d"))
-        {
-            transform.Translate (panSpeed * Vector3.right * Time.deltaTime);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            transform.Translate (panSpeed * Vector3.left * Time.deltaTime);
-        }
-
-        if (Input.GetKey("q"))
-        {
-            transform.Rotate (pivotSpeed * Vector3.down * Time.deltaTime);
-        }
-
-        if (Input.GetKey("e"))
-        {
-            transform.Rotate (pivotSpeed * Vector3.up * Time.deltaTime);
-        }
-
-
-
+        //scroll/zoom
         float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos = transform.position;
         pos.y -= scroll * scrollSpeed * 100f *  Time.deltaTime;
 
+        //clamp camera
         pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
         pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
 
-       //transform.position = pos;
-
-	}
+        transform.position = pos;
+    }
 }
