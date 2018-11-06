053ec63fe0ad6c3e7e13444e40f18d854e920ce6
UsRikkert <ricknicolai@gmail.com>
2018-11-06 13:45:57 +0100

    Achmed

--- Clash of the Empires/Assets/Scripts/UnitScripts/Achmed.cs
+++ Clash of the Empires/Assets/Scripts/UnitScripts/Achmed.cs
@@ -5,4 +5,51 @@ using UnityEngine;
 public class Achmed : OffensivePlaceables
 {
 
+    public AudioClip Detonate;
+    public GameObject explosion;
+    public bool inDetonateRange = false;
+    private CheckForEnemy checkForEnemyRef;
+    public bool isExploding = false;
+    AudioSource source;
+
+    private void Start()
+    {
+        checkForEnemyRef = GetComponentInChildren<CheckForEnemy>();
+        source = GetComponent<AudioSource> ();
+    }
+
+    void Update()
+    {
+        targetCheck();
+    }
+    void targetCheck()
+    {
+        GameObject target = checkForEnemyRef.mainTarget;
+        if (target != null && isExploding == false)
+        {
+            float distance = Vector3.Distance(target.transform.position, this.transform.position);
+            if (distance < 10)
+            {
+                
+                inDetonateRange = true;
+            }
+            if (target.GetComponent<Units>().currentTeam != this.currentTeam && inDetonateRange == true)
+            {
+                StartCoroutine(explode());
+                isExploding = true;
+            }
+        }
+    }
+
+    IEnumerator explode()
+    {
+        source.PlayOneShot(Detonate);
+        yield return new WaitForSeconds(1.5f);
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+
+
 }
