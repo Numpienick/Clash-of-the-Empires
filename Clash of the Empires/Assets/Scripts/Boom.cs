c00bb3d607ca0eec5b7befff406a15d42bb88fde
Rick Nicolai <ricknicolai@gmail.com>
2018-11-14 23:06:36 +0100

    yeet

--- Clash of the Empires/Assets/Scripts/Boom.cs
+++ Clash of the Empires/Assets/Scripts/Boom.cs
@@ -6,7 +6,7 @@ public class Boom : MonoBehaviour {
 
     public float expDmg = 150f;
     Placeables enemyVar;
-    // Use this for initialization
+    // Use this for initialization.
     private void Awake()
     {
         StartCoroutine(Explode());
@@ -15,17 +15,17 @@ public class Boom : MonoBehaviour {
 
     IEnumerator Explode()
     {
-        //Creates an array of colliders in range when the explosion effect is instantiated
+        //Creates an array of colliders in range when the explosion effect is instantiated.
         Collider[] colliders = Physics.OverlapSphere(transform.position, 75.0f);
         
 
         foreach (Collider collider in colliders)
         {
-        // Checks if the collider has a Rigidbody    
+        // Checks if the collider has the Placeable script.   
             if (enemyVar != null)
             {
                 enemyVar.GetComponentInParent<Placeables>().DealDamage(expDmg); 
-                // Deals damage according to the publicly set explosion damage variable
+                // Deals damage according to the publicly set explosion damage variable.
             }
 
         }
