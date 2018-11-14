0d2ae34c14bf199df5b9a6644e63a490bc7f9d7e
Nick Numan <211162@edu.rocfriesepoort.nl>
2018-11-14 22:58:56 +0100

    Revert "Confusion"
    This reverts commit 8787aefbede0c8d343cb8713f4bb15cfe26a4728.

--- Clash of the Empires/Assets/Scripts/Boom.cs
+++ Clash of the Empires/Assets/Scripts/Boom.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Boom : MonoBehaviour {
 
     public float expDmg = 150f;
-    Placeables enemyVar;
+    Rigidbody rb;
     // Use this for initialization
     private void Awake()
     {
@@ -16,15 +16,15 @@ public class Boom : MonoBehaviour {
     IEnumerator Explode()
     {
         //Creates an array of colliders in range when the explosion effect is instantiated
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 75.0f);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 15.0f);
         
 
         foreach (Collider collider in colliders)
         {
         // Checks if the collider has a Rigidbody    
-            if (enemyVar != null)
+            if (rb != null)
             {
-                enemyVar.GetComponentInParent<Placeables>().DealDamage(expDmg); 
+                rb.GetComponentInParent<Placeables>().DealDamage(expDmg); 
                 // Deals damage according to the publicly set explosion damage variable
             }
 
